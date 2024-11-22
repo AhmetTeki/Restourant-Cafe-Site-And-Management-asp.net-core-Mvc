8e3c45f7dc36f72da42d7572e87a8d0e19746a3e
Ahmet Tekin <ahmettekin023402@gmail.com>
2024-11-22 17:03:36 +0300

    son hali

---
+++ Cafe/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,67 @@
+﻿using Cafe.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cafe.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var user= _context.ApplicationUser.ToList();
+            var rol= _context.Roles.ToList();
+            var userRol= _context.UserRoles.ToList();
+            foreach(var item in user)
+            {
+                var roleId = userRol.FirstOrDefault(i => i.UserId == item.Id).RoleId;
+                item.Rol = rol.FirstOrDefault(u => u.Id == roleId).Name;
+            }
+            return View(user);
+        }
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.ApplicationUser == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.ApplicationUser
+                .FirstOrDefaultAsync(m => m.Id == id.ToString());
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // POST: Admin/Catagories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.ApplicationUser == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Catagory'  is null.");
+            }
+            var user = await _context.ApplicationUser.FindAsync(id);
+            if (user != null)
+            {
+                _context.ApplicationUser.Remove(user);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- Cafe/Program.cs
+++ Cafe/Program.cs
@@ -18,7 +18,13 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultTokenProvid
 builder.Services.AddSingleton<IEmailSender, EmailSender>();
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
-    
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = $"/Identity/Account/Login";
+    options.LogoutPath = $"/Identity/Account/Logout";
+    options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+});
+
 
 
 var app = builder.Build();
